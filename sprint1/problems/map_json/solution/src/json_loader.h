15929c5485f8c45cf674203a7aae47fee28570fa
shafrnv <shafrnv@gmail.com>
2024-07-24 12:55:35 +0300

    done map_json

---
+++ sprint1/problems/map_json/solution/src/json_loader.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+#include "model.h"
+
+namespace json = boost::json;
+using namespace model;
+namespace json_loader {
+
+Game LoadGame(const std::filesystem::path& json_path);
+
+
+}  // namespace json_loader

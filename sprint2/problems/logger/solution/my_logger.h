55e137d9356fc2eaddf4e3a61d50205c70f3ae88
shafrnv <shafrnv@gmail.com>
2024-07-26 03:34:19 +0300

    strange error during local tests

---
+++ sprint2/problems/logger/solution/my_logger.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <optional>
+#include <mutex>
+#include <thread>
+#include <iostream>
+
+using namespace std::literals;
+
+#define LOG(...) Logger::GetInstance().Log(__VA_ARGS__)
+
+class Logger {
+private:
+    std::ofstream log_file_;
+    std::string current_log_file_name_;
+    std::mutex mutex_;
+    std::optional<std::chrono::system_clock::time_point> manual_ts_;
+
+    Logger() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        OpenLogFile();
+    }
+
+    Logger(const Logger&) = delete;
+
+    auto GetTime() const {
+        if (manual_ts_) {
+            return *manual_ts_;
+        }
+        return std::chrono::system_clock::now();
+    }
+
+    auto GetTimeStamp() const {
+        const auto now = GetTime();
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
+        return std::put_time(std::gmtime(&t_c), "%F %T");
+    }
+
+    std::string GetFileTimeStamp() const {
+        const auto now = GetTime();
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
+        std::stringstream ss;
+        ss << std::put_time(std::gmtime(&t_c), "%Y_%m_%d");
+        return ss.str();
+    }
+
+    void OpenLogFile() {
+        const std::string log_file_name = "/var/log/sample_log_" + GetFileTimeStamp() + ".log";
+        if (log_file_name != current_log_file_name_) {
+            if (log_file_.is_open()) {
+                log_file_.close();
+            }
+            current_log_file_name_ = log_file_name;
+            log_file_.open(current_log_file_name_, std::ios::app);
+        }
+    }
+
+public:
+    static Logger& GetInstance() {
+        static Logger instance;
+        return instance;
+    }
+
+    template<class... Ts>
+    void Log(const Ts&... args) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        OpenLogFile();
+        std::stringstream ss;
+        ss << GetTimeStamp() << ": ";
+        (ss << ... << args);
+        ss << std::endl;
+        log_file_ << ss.str();
+    }
+
+    void SetTimestamp(std::chrono::system_clock::time_point ts) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        manual_ts_ = ts;
+    }
+};
